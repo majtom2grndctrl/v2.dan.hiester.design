2fcf12de5980f9f6eb54c0feacd06dc070d31f28
Dan Hiester <dan.hiester@gmail.com>
2023-11-17 23:33:40 -0800

    Add a default layout with a stub for a site footer

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <slot />
+  <footer class="SiteFooter--wrapper">
+    <div class="SiteFooter dhd--layout-wrapper">
+      Dan Hiester
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  .SiteFooter--wrapper {
+    background-color: gray;
+    padding-top: var(--spatial-scale-3);
+    padding-bottom: var(--spatial-scale-5);
+  }
+</style>
