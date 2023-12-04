dca9246c26c84f990141269266f633a8941d12c1
Dan Hiester <dan.hiester@gmail.com>
2023-12-03 18:10:21 -0800

    First pass at laying out footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup>
   import ContactCTA from '~/components/ContactCTA.vue';
+  import paths from '~/paths';
+  const prismic = usePrismic();
+  const navLinks = useAsyncData(
+    '$site_navigation',
+    () => prismic.client.getSingle('main_navigation')
+  ).data;
 </script>
 
 <template>
@@ -7,7 +13,19 @@
   <ContactCTA />
   <footer class="SiteFooter--wrapper">
     <div class="SiteFooter dhd--layout-wrapper">
-      <NuxtLink to="/" class="brand">Dan Hiester</NuxtLink>
+      <div class="brand-wrapper">
+        <NuxtLink :to="paths.home" class="brand">Dan Hiester</NuxtLink>
+      </div>
+      <nav class="nav-wrapper">
+        <PrismicLink
+          v-for="item in navLinks?.data.links"
+          :key="item.label + '_link'"
+          :field="item.link"
+          class="nav-link"
+        >
+          {{ item.label }}
+        </PrismicLink>
+      </nav>
     </div>
   </footer>
 </template>
@@ -24,14 +42,58 @@
       padding-bottom: var(--spatial-scale-8);
     }
   }
+  .SiteFooter {
+    display: grid;
+    grid-template-columns: repeat(15, 1fr);
+    @media (min-width: $breakpoint-royale) {
+      grid-template-columns: repeat(17, 1fr);
+    }
+  }
   a {
     color: var(--footer-text-color);
     text-decoration: none;
+  }
+  .brand-wrapper {
+    margin-bottom: var(--spatial-scale-3);
+    grid-column: 1 / span 8;
+    @media (min-width: $breakpoint-small) {
+      grid-column: 1 / span 7;
+    }
+    @media (min-width: $breakpoint-medium) {
+      grid-column: 1 / span 5;
+    }
+    @media (min-width: $breakpoint-large) {
+      grid-column: 1 / span 3;
+    }
+    @media (min-width: $breakpoint-royale) {
+      grid-column: 1 / span 3;
+    }
   }
   .brand {
     font-family: var(--font-heading);
     font-weight: 600;
     font-size: var(--type-scale-3);
     line-height: var(--spatial-scale-3);
+  }
+  .nav-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spatial-scale-00);
+    grid-column: 10 / span 7;
+    @media (min-width: $breakpoint-small) {
+      grid-column: 9 / span 5;
+    }
+    @media (min-width: $breakpoint-medium) {
+      grid-column: 6 / span 5;
+    }
+    @media (min-width: $breakpoint-large) {
+      grid-column: 4 / span 5;
+    }
+    @media (min-width: $breakpoint-royale) {
+      grid-column: 4 / span 5;
+    }
+  }
+  .nav-link {
+    display: inline-block;
   }
 </style>
