65580783f2df6b2d34819d8c64d1786e80be6f03
Dan Hiester <dan.hiester@gmail.com>
2023-12-12 11:06:06 -0800

    Fix an issue with portfolio routing

--- pages/portfolio/[uid].vue
+++ pages/portfolio/[uid].vue
@@ -4,7 +4,7 @@ import PageHero from '~/components/PageHero.vue'
 
 const prismic = usePrismic()
 const route = useRoute()
-const { data: page } = await useAsyncData('case_study[uid]', () =>
+const { data: page } = await useAsyncData(`case_study--${route.params.uid}`, () =>
   prismic.client.getByUID('case_study', route.params.uid as string)
 )
 useHead({
