8c58bf209c41d85745948058a7450f64084327d2
Dan Hiester <dan.hiester@gmail.com>
2025-09-29 19:14:39 -0700

    Upgrade Next 3 -> Nuxt 4

--- paths.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  home: '/',
-  about: '/about-dan',
-  articles: '/articles-and-presentations',
-  single_page: (uid: string) => `/${uid}`,
-  portfolio_page: (uid: string) => `/portfolio/${uid}`
-}
